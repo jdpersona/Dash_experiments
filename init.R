744154e38c3ac1755cd6006619a068096cab5591
Johnson  Ekedum <johnekedum@gmail.com>
2018-07-15 08:50:00 -0700

    Add files via upload

---
+++ init.R
@@ -0,0 +1,15 @@
+
+
+install.packages("shinythemes", dependencies=TRUE)  
+install.packages("shiny", dependencies=TRUE)  
+install.packages("plotly", dependencies=TRUE)  
+install.packages("ggplot2", dependencies=TRUE)  
+install.packages("dplyr", dependencies=TRUE) 
+install.packages("stringr", dependencies=TRUE) 
+install.packages("tidyr", dependencies=TRUE)
+install.packages("data.table", dependencies=TRUE)
+install.packages("magrittr", dependencies=TRUE)  
+install.packages("highcharter", dependencies=TRUE)  
+ 
+
+
