a6f2da6ca3caf8b80f3452989cdd48c2bb55fc07
Johnson  Ekedum <johnekedum@gmail.com>
2018-11-23 14:37:34 -0800

    Update init.r

--- init.r
+++ init.r
@@ -1,14 +1,7 @@
 
-
-install.packages("shinythemes", dependencies=TRUE)  
+ 
 install.packages("shiny", dependencies=TRUE)  
 install.packages("ggplot2", dependencies=TRUE)  
-install.packages("dplyr", dependencies=TRUE) 
-install.packages("stringr", dependencies=TRUE) 
-install.packages("tidyr", dependencies=TRUE)
-install.packages("magrittr", dependencies=TRUE)  
-install.packages("shinyauthr", dependencies=TRUE) 
-install.packages("shinyjs", dependencies=TRUE) 
 
 
 
