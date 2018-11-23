43a609496f65d71d72102a3f95cad4e416b3345a
Johnson  Ekedum <johnekedum@gmail.com>
2018-11-23 11:02:09 -0800

    Update app.R

--- app.R
+++ app.R
@@ -6,160 +6,83 @@
 #
 #    http://shiny.rstudio.com/
 #
-library(shinythemes)
 library(shiny)
-library(plotly)
+library(shinyauthr)
+library(shinyjs)
 library(ggplot2)
-library(dplyr)
-library(stringr)
-library(tidyr)
-library(data.table)
-library(magrittr)
-library(highcharter)
 
 
-data = read.csv('/Users/john.ekedum@ibm.com/Documents/Data Visualization/Shiny Apps/SAS RESponse/all_data.csv')
+# dataframe that holds usernames, passwords and other user data
+user_base <- data.frame(
+  user = c("user1", "user2"),
+  password = c("pass1", "pass2"), 
+  permissions = c("admin", "standard"),
+  name = c("User One", "User Two"),
+  stringsAsFactors = FALSE
+)
 
-
-# there are 319 dataset
-data %>% 
-  count()
-
-# there are 10 columns
-length(colnames(data))
-
-# convert character date to real dates
-data$Begin_Date = as.Date(data$Begin_Date, "%m/%d/%y")
-data$End_Date = as.Date(data$End_Date, "%m/%d/%y")
-
-sub_data = data %>% 
-  select(Begin_Date, url, form_shown, form_submitted) %>% 
-  group_by(Begin_Date, url) %>% summarise (Total_form_shown = sum(form_shown), Total_form_submitted = sum(form_submitted)) %>% 
-  arrange(desc(url))
-
-sub_data$Conversion_rate =  round((sub_data$Total_form_submitted/sub_data$Total_form_shown) * 100, digits=0)
-
-sub_data$url = str_trim(sub_data$url)
-
-
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-                
-                shinythemes::themeSelector(),
-  
-  # Application title
-  titlePanel("SaaS Response",windowTitle = "SaaS Exploration"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      width = 3,
-      selectInput(inputId ="choose", 
-                  choices = c("Watson Campaign Automation","tealeaf","digitalexperience","digitalanalytics"),    
-                  selected = "Watson Campaign Automation",label = "Choose a SaaS Product"),
-      br(),
-      br(),
-      br()
-     
-    
- 
-      
-    ),
-    
-    
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      
-      tabsetPanel(
-        tabPanel("Graphs", highchartOutput("plot",height = 400, width = 1000),
-                 br(),
-                 highchartOutput("plot2",height = 400, width = 1000),
-                 br(),
-                 highchartOutput("plot3",height = 400, width = 1000),
-                 br(),
-                 DT :: dataTableOutput('table')
-                 
-                 
-                 
-        ),
-        
-       tabPanel("Agregated Data", DT :: dataTableOutput('table2') )
-                 
-      )
-    )
-    
-  )
+data <- data.frame(
+  name=letters[1:5],
+  value=sample(seq(4,15),5),
+  sd=c(1,0.2,3,2,4)
 )
 
 
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$plot <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose,] 
-    
-    highchart() %>% 
-      hc_title(text = "Total Form Submitted") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Total_form_submitted)
-    
-  })
-  
+ui <- fluidPage(theme = 'cosmo',
+  # must turn shinyjs on
+  shinyjs::useShinyjs(),
+  # add logout button UI 
+  div(class = "pull-right", shinyauthr::logoutUI(id = "logout") ),
 
-  output$plot2 <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose, ] 
-    
-    highchart() %>% 
-      hc_title(text = "Total Form Shown") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Total_form_shown)
-  })
+  # add login panel UI function
+  div(class ="log",
+  shinyauthr::loginUI(id = "login")
+  ),
+  # setup table output to show user info after login
+  tableOutput("user_table"),
+  plotOutput('ggdisplay')
   
-  
-  output$plot3 <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose, ] 
-    
-    highchart() %>% 
-      hc_title(text = "Conversion Rate") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Conversion_rate)
-    
-  })
-  
-  
-  mytable <-  reactive({
-    df_WCA = sub_data[sub_data$url == input$choose, ]
-    
-    total_growth = round(df_WCA[df_WCA$Begin_Date == max(df_WCA$Begin_Date),]['Conversion_rate']-df_WCA[df_WCA$Begin_Date == min(df_WCA$Begin_Date),]['Conversion_rate'],0)
-    avg_conversion = round(sum(df_WCA$Conversion_rate)/length(df_WCA$Conversion_rate),0)
-    avg_form_sub = sum(df_WCA$Total_form_submitted)/length(df_WCA$Total_form_submitted)
-    avg_form_shown  = sum(df_WCA$Total_form_shown)/length(df_WCA$Total_form_shown)
-    test = data.frame( "Rate" = c(avg_form_shown, avg_form_sub, paste(avg_conversion,"percent"), paste(total_growth$Conversion_rate,"percent")), row.names = c('Average Form Shown', 'Average Form Submitted', 'Average Conversion rates', 'Total Growth') )
-    test
-  })  
-  
-  
-  
-  output$table <- DT :: renderDataTable({           
-    
-    DT::datatable(mytable(), rownames = TRUE, options = list(pageLength =4))
-  }, server=TRUE)
-  
-  
-  output$table2 <- DT :: renderDataTable({           
-    
-    DT::datatable(sub_data[sub_data$url == input$choose, ], rownames = TRUE, options = list(pageLength =10))
-  
-    }, server=TRUE)
-  
+)
 
+server <- function(input, output, session) {
+  
+  # call the logout module with reactive trigger to hide/show
+  logout_init <- callModule(shinyauthr::logout, 
+                            id = "logout", 
+                            active = reactive(credentials()$user_auth))
+  
+  # call login module supplying data frame, user and password cols
+  # and reactive trigger
+  credentials <- callModule(shinyauthr::login, 
+                            id = "login", 
+                            data = user_base,
+                            user_col = user,
+                            pwd_col = password,
+                            log_out = reactive(logout_init()))
+  
+  # pulls out the user information returned from login module
+  user_data <- reactive({credentials()$info})
+  
+  output$user_table <- renderTable({
+    # use req to only render results when credentials()$user_auth is TRUE
+    req(credentials()$user_auth)
+    user_data()
+  })
+  
+  
+ output$ggdisplay <- renderPlot({
+  # create dummy data
+
+  req(credentials()$user_auth)
+  ggplot(data) +
+    geom_bar( aes(x=name, y=value), stat="identity", fill="skyblue", alpha=0.7) +
+    geom_errorbar( aes(x=name, ymin=value-sd, ymax=value+sd), width=0.4, colour="orange", alpha=0.9, size=1.3)
+  
+ })
+  
   
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
 
+shinyApp(ui = ui, server = server)
