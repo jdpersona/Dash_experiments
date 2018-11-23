2ea9e3f03ef07c88079eb25627fed180169c0f94
Johnson  Ekedum <johnekedum@gmail.com>
2018-11-23 10:59:58 -0800

    Update init.R

--- init.R
+++ init.R
@@ -2,14 +2,13 @@
 
 install.packages("shinythemes", dependencies=TRUE)  
 install.packages("shiny", dependencies=TRUE)  
-install.packages("plotly", dependencies=TRUE)  
 install.packages("ggplot2", dependencies=TRUE)  
 install.packages("dplyr", dependencies=TRUE) 
 install.packages("stringr", dependencies=TRUE) 
 install.packages("tidyr", dependencies=TRUE)
-install.packages("data.table", dependencies=TRUE)
 install.packages("magrittr", dependencies=TRUE)  
-install.packages("highcharter", dependencies=TRUE)  
- 
+install.packages("shinyauthr", dependencies=TRUE) 
+install.packages("shinyjs", dependencies=TRUE) 
+
 
 
