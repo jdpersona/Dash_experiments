0e9c43963f871a411524cfc99934447fa9ca390b
Johnson  Ekedum <johnekedum@gmail.com>
2019-03-12 05:09:11 -0700

    Update init.r

--- init.r
+++ init.r
@@ -1,7 +1,13 @@
 
  
 install.packages("shiny", dependencies=TRUE)  
-install.packages("ggplot2", dependencies=TRUE)  
+install.packages("shinydashboard", dependencies=TRUE)  
+install.packages("leaflet", dependencies=TRUE)  
+install.packages("dplyr", dependencies=TRUE)  
+install.packages("lubridate", dependencies=TRUE)  
+install.packages("stringr", dependencies=TRUE)  
+install.packages("readr", dependencies=TRUE)  
+install.packages("billboarder", dependencies=TRUE)
 
 
 
